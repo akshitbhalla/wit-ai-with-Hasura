ed57217f6ccd4c51c3c88056872e6b5c07fefe94
Akshit Bhalla <akshitbhalla13@gmail.com>
2018-02-25 14:58:19 +0530

    Added POST to Flask backend

--- react-native/src/components/NavBar.js
+++ react-native/src/components/NavBar.js
@@ -5,13 +5,7 @@ import { Icon } from 'native-base';
 export default function NavBarCustom() {
   return (
     <NavBar>
-        <NavButton onPress={() => alert('Logout by shaking your phone and pressing Refresh (temporary fix)')}>
-            
-            <Icon name='arrow-back'/>
-            
-        </NavButton>
         <NavTitle>T61-HPDF</NavTitle>
-      <NavButton />
     </NavBar>
   );
 }
