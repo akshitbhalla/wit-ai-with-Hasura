e5a5cd02b00adb0879a2c37af969a6a09a27e194
Akshit Bhalla <akshitbhalla13@gmail.com>
2018-03-16 22:04:11 +0530

    Changed App name

--- react-native/App.js
+++ react-native/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Container } from 'native-base';
-import AuthScreen from './src/components/AuthScreen';
-import ChatScreen from './src/components/ChatScreen';
+import React from "react";
+import { Container } from "native-base";
+import AuthScreen from "./src/components/AuthScreen";
+import ChatScreen from "./src/components/ChatScreen";
 
 export default class App extends React.Component {
-  
   render() {
-    console.log("App started")
+    console.log("App started");
     return (
       <Container>
-        <AuthScreen logoutCallback = {this.backToLoginScreen}/>
+        <AuthScreen logoutCallback={this.backToLoginScreen} />
       </Container>
     );
   }
 }
-
--- react-native/src/components/data.js
+++ react-native/src/components/data.js
@@ -1,8 +1,8 @@
 export default [
   {
     _id: Math.round(Math.random() * 1000000),
-    text: 'This is a wit.ai integration with Hasura APIs',
+    text: "This is a wit.ai integration with Hasura APIs",
     createdAt: new Date(),
-    system: true,
+    system: true
   }
 ];
